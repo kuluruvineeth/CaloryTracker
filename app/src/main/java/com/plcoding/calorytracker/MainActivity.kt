5a08def9be2a1a9e2effffe2fe5fdf8a3fc2713e
kuluru vineeth kumar reddy <kuluruvineeth8623@gmail.com>
2022-09-18 13:47:06 +0530

    added search text field

---
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.plcoding.calorytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import coil.annotation.ExperimentalCoilApi
+import com.plcoding.calorytracker.navigation.navigate
+import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
+import com.plcoding.core.navigation.Route
+import com.plcoding.onboarding_presentation.activity.ActivityScreen
+import com.plcoding.onboarding_presentation.age.AgeScreen
+import com.plcoding.onboarding_presentation.gender.GenderScreen
+import com.plcoding.onboarding_presentation.goal.GoalScreen
+import com.plcoding.onboarding_presentation.height.HeightScreen
+import com.plcoding.onboarding_presentation.nutrient_goal.NutrientGoalScreen
+import com.plcoding.onboarding_presentation.weight.WeightScreen
+import com.plcoding.onboarding_presentation.welcome.WelcomeScreen
+import com.plcoding.tracker_presentation.search.SearchScreen
+import com.plcoding.tracker_presentation.tracker_overview.TrackerOverviewScreen
+import dagger.hilt.android.AndroidEntryPoint
+
+@ExperimentalComposeUiApi
+@ExperimentalCoilApi
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerTheme {
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HEIGHT) {
+                            HeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.WEIGHT) {
+                            WeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.ACTIVITY) {
+                            ActivityScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.GOAL) {
+                            GoalScreen(onNavigate = navController::navigate)
+                        }
+
+                        composable(Route.TRACKER_OVERVIEW) {
+                            TrackerOverviewScreen(
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                },
+                            )
+                        ) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                }
+                            )
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
